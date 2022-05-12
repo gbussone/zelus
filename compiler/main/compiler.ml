3bbf53e66fc8c3605d838efba77f8b4a5658cfd0
Grégoire BUSSONE <gregoire.bussone@ens.fr>
2022-05-12 14:35:26 +0200

    Add an option to enable the transformation

--- compiler/main/compiler.ml
+++ compiler/main/compiler.ml
@@ -135,8 +135,10 @@ let compile modname filename =
   let impl_list =
     step "Scoping done. See below:" Scoping.implementation_list impl_list in
   let impl_list =
-    step "Translation of probabilistic nodes for APF done. See below:"
-         Apf.implementation_list impl_list in
+    if !apf
+    then step "Translation of probabilistic nodes for APF done. See below:"
+              Apf.implementation_list impl_list
+    else impl_list in
   let impl_list =
     step "Typing done." (Typing.implementation_list info_ff true) impl_list in
   let impl_list =
