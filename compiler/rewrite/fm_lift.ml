c7664302e921d5b8e8467e5c8a74486af78be795
Grégoire BUSSONE <gregoire.bussone@ens.fr>
2022-10-03 11:15:39 +0200

    Write a correct type system for liftable expressions

--- compiler/rewrite/fm_lift.ml
+++ compiler/rewrite/fm_lift.ml
@@ -27,22 +27,25 @@ open Zelus
 
 let rec expression e =
   match e.e_desc with
-  | Elocal _
-  | Eglobal _
-  | Econst _
-  | Econstr0 _
-  | Econstr1 _
-  | Elast _
-  | Eapp _
-  | Eop _
-  | Etuple _
-  | Erecord_access _
-  | Erecord _
-  | Erecord_with _
-  | Etypeconstraint _
-  | Epresent _
-  | Ematch _
-  | Elet _
-  | Eseq _
-  | Eperiod _
-  | Eblock _ -> true (* TODO *)
+  | Elocal _ -> false
+  | Eglobal _ -> true
+  | Econst _ -> true
+  | Econstr0 _ -> true
+  | Econstr1 (_, e_list) -> List.for_all expression e_list
+  | Elast _ -> false
+  | Eapp (_, op, [obs]) when not (Ztypes.is_probabilistic 0 op.e_typ) ->
+     expression obs
+  | Eapp _ -> false
+  | Eop (_, e_list) -> List.for_all expression e_list
+  | Etuple e_list -> List.for_all expression e_list
+  | Erecord_access (e, _) -> expression e
+  | Erecord l_e_list -> List.for_all (fun (_, e) -> expression e) l_e_list
+  | Erecord_with (e, l_e_list) ->
+     expression e && List.for_all (fun (_, e) -> expression e) l_e_list
+  | Etypeconstraint (e, _) -> expression e
+  | Epresent _ -> false
+  | Ematch _ -> false
+  | Elet _ -> false
+  | Eseq (e1, e2) -> expression e1 && expression e2
+  | Eperiod _ -> false
+  | Eblock _ -> false
